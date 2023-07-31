3467bb95b894f13efc50fc4aa7cc06a506c3be99
sinoxoloxakile <athandiwexakile@gmail.com>
2023-07-31 23:07:11 +0200

    Prime Number Function

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,10 +1,20 @@
 def is_prime(number)
-    if n <= 1:
-        return False
-
-    for i in range(2, n):
-        if (n % i != 0):
-            continue
-        else:
-            return False
-    return True
+     """
+  This function takes a number as input and returns True if the number is prime, and False otherwise.
+  Args:
+    number: The number to check for primality.
+  Returns:
+    True if the number is prime, and False otherwise.
+  """
+  # Check if the number is 1.
+  if number == 1:
+    return False
+  # Check if the number is even.
+  if number % 2 == 0:
+    return False
+  # Check if the number is divisible by any number from 3 to the square root of the number.
+  for i in range(3, int(number ** 0.5) + 1, 2):
+    if number % i == 0:
+      return False
+  # If the number is not divisible by any number from 2 to the square root of the number, then it is prime.
+  return True
