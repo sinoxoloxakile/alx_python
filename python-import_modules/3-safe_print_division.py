a4c960da85fd42ef6e6674ab0239fd49fbead4dc
sinoxoloxakile <athandiwexakile@gmail.com>
2023-07-31 20:51:56 +0200

    Integers division with debug

---
+++ python-import_modules/3-safe_print_division.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+ def safe_print_division(a, b):
+  """
+  This function divides two integers and prints the result.
+  Args:
+    a: The first integer to divide.
+    b: The second integer to divide by.
+  Returns:
+    The value of the division, or `None` if an error occurred.
+  """
+  try:
+    result = a / b
+  except ZeroDivisionError:
+    result = None
+  finally:
+    print("Inside result:", result)
+  return result
