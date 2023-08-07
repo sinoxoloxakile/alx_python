2c3b2caba3a07a24f3db92703fcada44128c343c
sinoxoloxakile <athandiwexakile@gmail.com>
2023-08-07 23:47:38 +0200

    Size validation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,6 +1,32 @@
+#!/usr/bin/python3
+"""
+class to square element
+"""
+
 class Square:
-  def __init__(self, size=0):
-    if not isinstance(size, int):
-      raise TypeError("size must be an integer")
-    if size < 0:
-      raise ValueError("size must be >= 0")
+    """
+    this class to representing a square
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a Square class
+        """
+        self.__size = self.check_int(size)
+
+    def check_int(self, size):
+        """
+        to check if the number in int
+        """
+        if isinstance(size, int):
+            return (self.check_negative(size))
+        else:
+            raise TypeError("size must be an integer")
+
+    def check_negative(self, size):
+        """
+        to check the number is negatev
+        """
+        if size >= 0:
+            return (size)
+        else:
+            raise ValueError("size must be >= 0")
