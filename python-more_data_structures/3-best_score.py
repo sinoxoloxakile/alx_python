5ac368826c2c2a1ca0ac8f70f7cc9f4413423ce3
sinoxoloxakile <athandiwexakile@gmail.com>
2023-08-07 23:27:36 +0200

    Best score

---
+++ python-more_data_structures/3-best_score.py
@@ -0,0 +1,8 @@
+def best_score(a_dictionary):
+  max_score = -1
+  best_key = None
+  for key, value in a_dictionary.items():
+    if value > max_score:
+      max_score = value
+      best_key = key
+  return best_key
