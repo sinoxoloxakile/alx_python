9d2afa783efac3af572f87b7b9bb76d8ecf6a1b0
sinoxoloxakile <athandiwexakile@gmail.com>
2023-08-20 10:46:20 +0200

    What's my status? #1

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+import request package
+"""
+import requests
+
+
+def main():
+    url = 'https://alu-intranet.hbtn.io/status'
+    req = requests.get(url)
+    print("Body response:\n\t- type: {}\n\t- content: {}".format(type(req.text), req.text))
+
+
+if __name__ == "__main__":
+    main()
