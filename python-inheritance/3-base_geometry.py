8aee30e250387b7fc8bc009dc42ad6c19352d1f3
sinoxoloxakile <athandiwexakile@gmail.com>
2023-08-20 09:58:13 +0200

    Geometry module

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,13 @@
+"""
+    this is a semple class to represent a empty class
+"""
+class BaseGeometry:
+    """
+    this is a semple class to represent a empty class
+    """
+    def __dir__(cls):
+        """
+        this is a semple class to represent a empty class
+        """
+        attributes = super().__dir__()
+        return [attr for attr in attributes if attr != '__init_subclass__']
