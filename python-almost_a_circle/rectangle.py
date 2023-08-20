10c364c30a379a360b098f320cb296e64821b691
sinoxoloxakile <athandiwexakile@gmail.com>
2023-08-20 10:41:49 +0200

    First Rectangle

--- python-almost_a_circle/rectangle.py
+++ python-almost_a_circle/rectangle.py
@@ -1,31 +1,114 @@
-class Rectangle(Base):
+#!/usr/bin/python3
+"""
+Module on a Rectangle class that inherits from Base
+"""
+from models.base import Base
+
+class Rectangle (Base):
+    """
+    This is the calss defining the Rectangle
+
+    Attribute include:
+    __width -> width
+    __height -> height
+    """
+
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        This function intializes a Instance of the Rectangle
+
+        Args:
+         width (int): The width of the rectangle.
+        height (int): The height of the rectangle.
+        x (int): The x-coordinate of the rectangle.
+        y (int): The y-coordinate of the rectangle.
+        id (int): The value for the id attribute.
+        """
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
+
     @property
     def width(self):
         return self.__width
+
     @width.setter
-    def width(self, width):
-        self.__width = integer_validator(width)
+    def width(self, value):
+        # Implement any validation logic you need
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
-    def height(self, height):
-        self.__height = integer_validator(height)
+    def height(self, value):
+        # Implement validation logic you need
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
     @property
     def x(self):
         return self.__x
+
     @x.setter
-    def x(self, x):
-        self.__x = integer_validator(x)
+    def x(self, value):
+        # Implement validation logic you need
+        if not isinstance(value, int):
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
     @property
     def y(self):
         return self.__y
+
     @y.setter
-    def y(self, y):
-        self.__y = integer_validator(y)
+    def y(self, value):
+        # Implement validation logic you need
+        if not isinstance(value, int):
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
+
+    def area(self):
+        """ Function for the area of Rectangle"""
+        return self.__width * self.__height
+    
+    def display(self):
+        """ Function for the display  """
+        for _ in range(self.__y):
+            print()
+        for _ in range(self.__height):
+            print(" " * self.__x + "#" * self.__width)
+    
+    def __str__(self):
+        return f"[Rectangle] ({self.id}) {self.__x}/{self.__y} - {self.__width}/{self.__height}"
+    
+    def update(self, *args, **kwargs):
+        """This function that assigns argument"""
+        if len(args) > 0:
+            self.id = args[0]
+        if len(args) > 1:
+            self.width = args[1]
+        if len(args) > 2:
+            self.height = args[2]
+        if len(args) > 3:
+            self.x = args[3]
+        if len(args) > 4:
+            self.y = args[4]
+        
+        if len(args) == 0 or len(kwargs) > 0:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
