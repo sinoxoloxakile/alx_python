b43f9d1fa7fc8d53e94949e1d4445f793c581911
sinoxoloxakile <athandiwexakile@gmail.com>
2023-08-20 10:02:45 +0200

    Geometry module

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -1,7 +1,13 @@
-class BaseGeometry:
-    def __init__(self):
-        pass
-    def area(self):
-        raise NotImplementedError
-    def perimeter(self):
-        raise NotImplementedError
+"""
+    this is a semple class to represent a empty class
+"""
+class BaseGeometry
+    """
+    this is a semple class to represent a empty class
+    """
+    def __dir__(cls):
+        """
+        this is a semple class to represent a empty class
+        """
+        attributes = super().__dir__()
+        return [attr for attr in attributes if attr != '__init_subclass__']
