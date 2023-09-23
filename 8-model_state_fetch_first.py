1e4d26cde09f52e24ada6d90b5d122aea98273c2
sinoxoloxakile <athandiwexakile@gmail.com>
2023-09-23 11:09:02 +0200

    First state

---
+++ 8-model_state_fetch_first.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql username> <mysql password> <database name>".format(sys.argv[0]))
+        sys.exit(1)
+    username, password, db_name = sys.argv[1:]
+    engine = create_engine('mysql+pymysql://{}:{}@localhost:3306/{}'.format(username, password, db_name))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).first()
+    if state:
+        print(state.id, state.name)
+    else:
+        print("Nothing")
